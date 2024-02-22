fc2389d218310339d4b2c8823f6c01d048fcf89a
Max Hertel <maxherteljr@gmail.com>
2024-02-22 10:14:44 -0300

    mudado porta do hello world

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //Mudei a porta para não haver conflito pois será a porta da api
+  await app.listen(3002);
 }
 bootstrap();
